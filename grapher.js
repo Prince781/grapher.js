46ab482f017153710f35878c3462ee00a86c665a
Prince781 <princetonf@optonline.net>
2014-02-14 21:55:24 -0500

    Working on rendering.

--- grapher.js
+++ grapher.js
@@ -9,7 +9,22 @@
 var Grapher = new function() {
 	var _this = this;
 	
-	this.
+	// specific render functions:
+	this.renderers = {
+		xy: function(ct) {
+			// ct - context
+			/* draws xy axes for a scatter/box plot */
+			this.drawAxes = function(pos, width, height) {
+				// pos - {x: num, y: num}
+				ct.beginPath();
+				ct.moveTo(pos.x, pos.y-height);
+				ct.lineTo(pos.x, pos.y);
+				ct.lineTo(pos.x+width, pos.y);
+				ct.stroke();
+				ct.closePath();
+			};
+		}
+	};
 	
 	/* renders a particular Graph (termed "GraphModel" here) */
 	this.render = function(GraphModel, options) {
@@ -21,7 +36,7 @@ var Grapher = new function() {
 			GraphModel.rCallback();
 		} catch (e) {
 			try { GraphModel.eCallback(); } catch (e) {} // callback
-			console.log("GraphModel error: "+e.description);
+			console.log("GraphModel error: "+e.message);
 		}
 	};
 };
@@ -36,26 +51,44 @@ var Graph = function(canvas, type, dataModel, options) {
 	 */
 	// rCallback - (optional) called after rendering has completed
 	// eCallback - (optional) potentially called after an error
-	if (canvas.tagName != "canvas")
+	if (canvas.tagName != "CANVAS")
 		throw "GraphModel: param \"canvas\" is not actually a <canvas> element.";
 
 	var _gthis = this;
 	
 	// private:
+	function getOption(key, def) {
+		// def - default value (if key is unavailable)
+		var ops = typeof options == "undefined" ? {} : options;
+		return (key in ops) ? ops[key] : def;
+	}
 	var ctx = canvas.getContext("2d");
+	// format: r_<type> denotes a renderer of type "<type>"
+	var r_xy = new Grapher.renderers.xy(ctx); // create a new xy renderer
+	
+	// default render functions:
+	function fRenderer() {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+	}
 	
 	this.type = type;
 	this.idata = []; // contains independent variables/labels
 	this.ddata = []; // contains dependent variables/labels
 	
-	this.rCallback = typeof rCallback == "function" ? rCallback : function(){}; // on successful render
-	this.eCallback = typeof eCallback == "function" ? eCallback : function(){}; // on error
+	this.rCallback = getOption("rCallback", function(){}); // on successful render
+	this.eCallback = getOption("eCallback", function(){}); // on error
 	
 	// dataModel varies between types
 	switch (type) {
 		case "scatter":
 			_gthis.render = function() {
 				fRenderer(); // do this first
+				//ctx.strokeStyle = "rgba(0,0,0,1)";
+				
+				
+				ctx.lineWidth = getOption("axesWidth", 2);
+				ctx.strokeStyle = getOption("axesColor", "rgba(124,124,124,0.95)");
+				r_xy.drawAxes({x:60.5,y:canvas.height-60.5}, canvas.width-100, canvas.height-100);
 			};
 			break;
 		default:
